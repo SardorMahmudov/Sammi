87cb4c4a745bf6e12cfb5d3e1ec3ccf05e3528d5
Sardor <ozdemeir09@gmail.com>
2024-02-24 06:41:15 +0500

    HOC, Header & Sidebar

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import "@fontsource/roboto";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { withLayout } from "../layout/layout";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default withLayout(Home);

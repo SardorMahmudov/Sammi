32d2ec22b1d9cc12b98591683e5aa7658a01c145
Sardor <ozdemeir09@gmail.com>
2024-02-25 08:56:16 +0500

    Books page

--- src/page-component/index.ts
+++ src/page-component/index.ts
@@ -1,2 +1,3 @@
 export { default as HomePageComponent } from "./home-page-component/home-page-component";
 export { default as CoursesPageComponent } from "./courses-page-component/courses-page-component";
+export { default as BookPageComponent } from "./book-page-component/book-page-component";
---
+++ src/pages/books.tsx
@@ -0,0 +1,8 @@
+import { withLayout } from "../layout/layout";
+import { BookPageComponent } from "../page-component";
+
+const Books = () => {
+  return <BookPageComponent />;
+};
+
+export default withLayout(Books);
